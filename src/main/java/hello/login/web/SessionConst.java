d72394c5d863e8025ca0fc981ca8617d41d2781a
Ik Hyeon Cho <andantej99@ajou.ac.kr>
2023-11-13 22:34:05 +0900

    세션 활용해 로그인 처리 개발 완료

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,6 @@
+package hello.login.web;
+
+public class SessionConst {
+    // 상수만 가져다가 쓰려고 만든 class -> interface로 구현하는게 맞음
+    public static final String LOGIN_MEMBER = "loginMember";
+}
---
+++ src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,68 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class SessionManager {
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+    /**
+     * 세션 생성
+     * * sessionId 생성(임의의 추정 불가능한 랜덤 값)
+     * * 세션 저장소에 sessionId와 보관할 값 저장
+     * * sessionId로 응답 쿠키를 생성해서 클라에게 전달
+     */
+
+    public void createSession(Object value, HttpServletResponse response) {
+
+        // 1. sessionId를 생성하고 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        // 2. 쿠키를 생성해 클라이언트에게 전달
+        Cookie cookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(cookie);
+    }
+
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null) {
+            return null;
+        }
+        
+        // sessionCookie.getValue() = UUID로 만든 sessionId 값을 의미함
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    public Cookie findCookie(HttpServletRequest request, String cookieName) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+
+        //배열을 stream으로 바꿔서 돌리기
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+    }
+
+
+    /**
+     * 세션 만료
+     */
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+}
